bb9c4335b2f66dc55057a532de3b0c5f9e4be42f
ShamilRahmanPK <shamilrahman7994@gmail.com>
2025-12-09 12:52:09 +0400

    added paper type option

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,3 @@
-const SERVER_BASE_URL = "http://localhost:5000"
-// const SERVER_BASE_URL = "https://inekas-assesment-server.onrender.com"
+// const SERVER_BASE_URL = "http://localhost:5000"
+const SERVER_BASE_URL = "https://inekas-assesment-server.onrender.com"
 export default SERVER_BASE_URL
